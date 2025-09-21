56e32668e3e4a6bc8f555048e3985381c2286dae
mahipujari301093 <mahipujari2024@gmail.com>
2025-09-21 15:00:14 +0530

    New commited for jave practice

---
+++ src/datatype/String_Practice.java
@@ -0,0 +1,15 @@
+package datatype;
+
+public class String_Practice {
+
+	public static void main(String[] args) {
+
+		String s1 = "Vishal";
+		String s2 = "Pujari";
+
+		System.out.println(s1 + " " + s2);
+		System.out.println(s1 + ' ' + s2);
+
+	}
+
+}
