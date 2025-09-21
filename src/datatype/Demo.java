adaeea89a9ebf281294201337654ada441eea72b
mahipujari301093 <mahipujari2024@gmail.com>
2025-09-21 15:52:58 +0530

    second code

---
+++ src/datatype/Demo.java
@@ -0,0 +1,14 @@
+package datatype;
+
+public class Demo {
+	
+	public static void main(String[]args)
+	{
+		
+		String commit = " I'm sharing my code to Avinash for review";
+		
+		System.out.println(commit);
+		
+	}
+
+}
