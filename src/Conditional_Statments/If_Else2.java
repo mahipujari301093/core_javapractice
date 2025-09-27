c766bbf860c98c33d3a8b262436f7858b567d249
mahipujari301093 <mahipujari2024@gmail.com>
2025-09-27 20:27:25 +0530

    Conditional statment

---
+++ src/Conditional_Statments/If_Else2.java
@@ -0,0 +1,28 @@
+package Conditional_Statments;
+
+public class If_Else2 {
+	
+	public static void main(String[] args) {
+		
+		int a = 100;
+		int b = 200;
+		
+		if(a<=b)
+		{
+			
+			System.out.println("Condition true");
+		}
+		
+		
+		else
+		{
+			
+			System.out.println("Condition false");
+			
+		}
+		
+		
+		
+		
+	}
+}
