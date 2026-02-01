13a5e02023e02363cfcf27d702433b39952f172c
mahipujari301093 <mahipujari2024@gmail.com>
2026-02-01 21:56:38 +0530

    Data type code added

---
+++ src/datatype/Notes.java
@@ -0,0 +1,69 @@
+package datatype;
+
+public class Notes {
+
+/*
+
+1️⃣ Primitive Data Types : Primitive data types store actual values.
+int a = 10;
+double d = 10.5;
+char c = 'A';
+boolean flag = true;
+
+Primitive types (8):
+byte, short, int, long, float, double, char, boolean
+
+*************************************************************************************
+
+2️⃣ Non-Primitive Data Types
+they store references to objects in memory rather than actual values.
+Examples of Non-Primitive Data Types:
+1️⃣ String
+
+String name = "Vishal";
+
+
+2️⃣ Arrays
+int[] numbers = {1, 2, 3, 4, 5};
+
+
+Simple rule to remember ⭐
+
+Primitive = stores actual value
+Non-primitive = stores reference to value
+
+
+
+What is Actual Value?
+👉 Actual value means the variable directly stores the data.
+Example (Primitive)
+int a = 10; // 'a' directly holds the value 10
+
+a stores 10
+No memory address reference
+Independent copy
+
+
+What is Reference Value?
+👉 Reference value means the variable stores the address of the object in memory.
+Example (Non-Primitive)
+String name = "Vishal"; // 'name' holds the reference to the String object
+name stores memory address of "Vishal"
+
+
+| Data Type | Size    | Example                |
+| --------- | ------- | ---------------------- |
+| byte      | 1 byte  | `byte b = 10;`         |
+| short     | 2 bytes | `short s = 100;`       |
+| int       | 4 bytes | `int i = 1000;`        |
+| long      | 8 bytes | `long l = 100000L;`    |
+| float     | 4 bytes | `float f = 10.5f;`     |
+| double    | 8 bytes | `double d = 10.55;`    |
+| char      | 2 bytes | `char c = 'A';`        |
+| boolean   | 1 bit   | `boolean flag = true;` |
+
+
+
+ */
+
+}
---
+++ src/datatype/Practice.java
@@ -0,0 +1,30 @@
+package datatype;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        // Primitive Data Types
+        int a = 25;
+        double d = 45.67;
+        char c = 'Z';
+        boolean b = false;
+
+        System.out.println("Primitive Data Types:");
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + d);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + b);
+
+        // Non-Primitive Data Types
+        String name = "John Doe";
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Non-Primitive Data Types:");
+        System.out.println("String: " + name);
+        System.out.println("Integer: " +numbers[0] + ", " + numbers[1] + ", " + numbers[2] + ", " + numbers[3] + ", " + numbers[4]);
+
+    }
+
+
+}
